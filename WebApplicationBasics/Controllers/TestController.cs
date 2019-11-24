f071383a6c6764c2ab7c73537823a38160e96398
Andrzej Gis <xaphones@gmail.com>
2019-11-24 21:43:20 +0100

    Empty project

---
+++ WebApplicationBasics/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationBasics.Controllers
+{
+    public class TestController : ControllerBase
+    {
+        [HttpGet("api/hello")]
+        public string HelloWorld()
+        {
+            return "Hello world!";
+        }
+
+        [HttpGet("api/sum")]
+        public int Get(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+    }
+}
